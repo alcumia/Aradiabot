18ac81504c0767277ad4e9bf5abb05ec3fceb5d1
alcumia <34531581+alcumia@users.noreply.github.com>
2017-12-14 00:20:24 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,26 +11,25 @@ client.on('message', message => {
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-
 var comment_box_cID = '390709196795936788';
 var comment_box_sub_cID = '390717626420166669';
 
 var admin_roleID = '351594043337342978';
 
-client.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', message => {
     // If they post in #comment-box, and they AREN'T an admin.
-	if (channelID === comment_box_cID) {
+	if (message.channel.id === comment_box_cID) {
     //if (channelID == comment_box_cID && !(message.author.hasRole(admin_roleID))) {
         var new_message = message.author.username+" submitted the following comment:\n"+message.content;
        
-        bot.sendMessage({
-                    to: comment_box_sub_cID,
-                    message: new_message
+        client.send(new_message, {
+                    to: comment_box_sub_cID
         });
 		
 		message.delete();
      }
 });
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
 */
